9f911fcf94d93585fc20c9150420003814495496
root <hippolytebourban@gmail.com>
2022-11-16 17:20:09 +0100

    solved

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true, in: (0..5) }
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5)}
 end
